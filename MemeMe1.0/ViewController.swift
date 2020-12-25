ea6d930cc6c5387dcd7eb57d79469ea23fa457e8
Phu Huynh <mrphuwin@gmail.com>
2020-12-25 02:00:17 -0800

    Added TOP and BOTTOM text fields

--- MemeMe1.0/ViewController.swift
+++ MemeMe1.0/ViewController.swift
@@ -10,10 +10,31 @@ import UIKit
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     @IBOutlet weak var cameraButton: UIBarButtonItem!
     @IBOutlet weak var imagePickerView: UIImageView!
+    @IBOutlet weak var topTextField: UITextField!
+    @IBOutlet weak var bottomTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        func setupTextField(_ button: UITextField, _ text: String) {
+            let memeTextAttributes: [NSAttributedString.Key: Any] = [
+//                NSAttributedString.Key.strokeColor: UIColor.black,
+//                NSAttributedString.Key.backgroundColor: UIColor.red,
+                NSAttributedString.Key.foregroundColor: UIColor.white,
+                NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+                NSAttributedString.Key.strokeWidth: 5
+            ]
+            
+            button.defaultTextAttributes = memeTextAttributes
+            button.textAlignment = .center
+            button.text = text
+            button.delegate = self
+        }
+        
+        setupTextField(topTextField, "TOP")
+        setupTextField(bottomTextField, "BOTTOM")
     }
+    
     
     override func viewWillAppear(_ animated: Bool) {
         cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
@@ -45,3 +66,24 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
 }
 
+extension ViewController: UITextFieldDelegate {
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        textField.text = ""
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        // Construct the text that will be in the field if this change is accepted
+        var newText = textField.text! as NSString
+        newText = newText.replacingCharacters(in: range, with: string) as NSString
+        
+        textField.text = (newText as String).uppercased()
+        
+        return false
+    }
+}
