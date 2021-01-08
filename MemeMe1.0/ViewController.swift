850cd5c1b85c9a9892d65e9c753208133e8786a2
Phu Huynh <mrphuwin@gmail.com>
2021-01-08 00:07:09 -0800

    Fixed font colors

--- MemeMe1.0/ViewController.swift
+++ MemeMe1.0/ViewController.swift
@@ -26,11 +26,10 @@ class ViewController: UIViewController {
         
         func setupTextField(_ button: UITextField) {
             let memeTextAttributes: [NSAttributedString.Key: Any] = [
-//                NSAttributedString.Key.strokeColor: UIColor.black,
-//                NSAttributedString.Key.backgroundColor: UIColor.red,
+                NSAttributedString.Key.strokeColor: UIColor.black,
                 NSAttributedString.Key.foregroundColor: UIColor.white,
                 NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
-                NSAttributedString.Key.strokeWidth: 5
+                NSAttributedString.Key.strokeWidth: -6.0
             ]
             
             button.defaultTextAttributes = memeTextAttributes
